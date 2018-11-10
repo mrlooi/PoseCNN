76fe3f68bd6ef7a3e6a44aa2f94926a4c1ea11c0
mrlooi <looicj@msn.com>
2018-11-10 18:40:32 +0800

    updated loss_pth

--- my_tools/loss_pth.py
+++ my_tools/loss_pth.py
@@ -5,43 +5,17 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+import tensorflow as tf
+
 sys.path.append("lib/model")
 from average_distance_loss.modules.average_distance_loss import AverageDistanceLoss
+from hard_label.modules.hard_label import HardLabel
+from hough_voting.modules.hough_voting import HoughVoting
 
 sys.path.append("lib")
 import hard_label_layer.hard_label_op as hard_label_op
 import hard_label_layer.hard_label_op_grad
 
-class persistent_locals(object):
-    def __init__(self, func):
-        self._locals = {}
-        self.func = func
-
-    def __call__(self, *args, **kwargs):
-        def tracer(frame, event, arg):
-            if event=='return':
-                l = frame.f_locals.copy()
-                self._locals = l
-                for k,v in l.items():
-                    globals()[k] = v
-
-        # tracer is activated on next call, return or exception
-        sys.setprofile(tracer)
-        try:
-            # trace the function call
-            res = self.func(*args, **kwargs)
-            
-        finally:
-            # disable tracer and replace with old one
-            sys.setprofile(None)
-        return res
-
-    def clear_locals(self):
-        self._locals = {}
-
-    @property
-    def locals(self):
-        return self._locals
 
 def loss_cross_entropy_single_frame(scores, labels):
     """
@@ -68,8 +42,22 @@ def smooth_l1_loss_vertex(vertex_pred, vertex_targets, vertex_weights, sigma=1.0
     loss = torch.div( torch.sum(in_loss), torch.sum(vertex_weights).float() )
     return loss
 
-def hard_label_func(prob_normalized, gt_label_2d, threshold=1.0):
+def hard_label_func_tf(prob_normalized, gt_label_2d, threshold=1.0):
     return hard_label_op.hard_label(prob_normalized, gt_label_2d, threshold, name="hard_label")
+
+def hard_label_func(prob_normalized, gt_label_2d, threshold=1.0):
+    return HardLabel(threshold)(gt_label_2d, prob_normalized)
+
+def run_hough_voting(num_classes, label_2d, vertex_pred, extents, poses, mdata):
+    vote_threshold = -1.0
+    vote_percentage = 0.02
+    skip_pixels = 20
+    label_threshold = 500
+    inlier_threshold = 0.9
+    is_train = True
+    hv = HoughVoting(num_classes, vote_threshold, vote_percentage, label_threshold=label_threshold, 
+            inlier_threshold=inlier_threshold, skip_pixels=skip_pixels, is_train=is_train)
+    return hv(label_2d, vertex_pred, extents, poses, mdata)
 
 def average_distance_loss_func(poses_pred, poses_target, poses_weight, points, symmetry, num_classes, margin=0.01):
 
@@ -89,10 +77,18 @@ def FCT(x, requires_grad=False):
 def ICT(x, requires_grad=False):
     return CT(x, dtype=torch.int32, requires_grad=requires_grad)
 
-if __name__ == '__main__':
-    import tensorflow as tf
+def get_error(d1, d2):
+    e = np.abs(d1-d2)
+    print(np.sum(e))
 
-    labels = np.load("labels.npy")
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.1)
+gpu_config = tf.ConfigProto(allow_soft_placement=True, gpu_options=gpu_options)
+cpu_config = tf.ConfigProto(device_count = {'GPU': 0})
+
+if __name__ == '__main__':
+
+
+    labels = np.load("labels.npy").astype(np.int32)
     vertex_targets = np.load("vertex_targets.npy")
     vertex_weights = np.load("vertex_weights.npy")
     poses_targets = np.load("poses_targets.npy")
@@ -103,6 +99,10 @@ if __name__ == '__main__':
     poses_pred = np.load("poses_pred.npy")
 
     points = np.load("points_all.npy")
+    poses_gt =  np.load("pose_gt.npy")
+    extents =  np.load("extents.npy")
+    meta_data =  np.load("meta_data.npy")
+
     symmetry = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1])
 
     # labels = np.squeeze(labels, axis=0)
@@ -114,25 +114,43 @@ if __name__ == '__main__':
     width = 640
     height = 480
 
+    t_score = FCT(score, True)
+    prob = F.log_softmax(t_score, 3)
+    prob_normalized = F.softmax(t_score, 3)
+    label_2d = torch.argmax(prob_normalized, dim=3)
+    # label_2d = FCT(label_2d, True)
+    hard_label_tf = hard_label_func_tf(prob_normalized.clone().detach().cpu().numpy(), labels)
 
-    prob = F.log_softmax(T(score), 3)
-    prob_normalized = F.softmax(T(score), 3)
-    hard_label_tf = hard_label_func(prob_normalized.detach().numpy(), labels)
+    sess = tf.Session(config=cpu_config)
+    sess.run(tf.global_variables_initializer())
+    hl_tf = sess.run(hard_label_tf)
+    hl_pth = hard_label_func(prob_normalized, ICT(labels)).detach().cpu().numpy()
+    get_error(hl_tf, hl_pth)
 
-    sess = tf.Session(config=tf.ConfigProto(device_count = {'GPU': 0}))
-    hard_label = sess.run(hard_label_tf)
-
-    t_hl = FT(hard_label, True)
-    t_vp = FT(vertex_pred, True)
-    t_vt = FT(vertex_targets)
-    t_vw = FT(vertex_weights)
+    # loss cross entropy
+    t_hl = FCT(hl_tf, True)
+    # loss vertex
+    t_vp = FCT(vertex_pred, True)
+    t_vt = FCT(vertex_targets)
+    t_vw = FCT(vertex_weights)
+    # loss hough vote
+    t_pgt = FCT(poses_gt)
+    t_extents = FCT(extents)
+    t_meta = FCT(meta_data)
+    # loss pose
     t_pp = FCT(poses_pred, True)
     t_pt = FCT(poses_targets)
     t_pw = FCT(poses_weights)
     t_pts = FCT(points)
     t_sym = FCT(symmetry)
 
+    hough_outputs = run_hough_voting(num_classes, label_2d, t_vp, t_extents, t_pgt, t_meta)
+    t_pt, t_pw = hough_outputs[2:4]
+
     # loss_cls = loss_cross_entropy_single_frame(prob, t_hl)
     # loss_vertex = VERTEX_W * smooth_l1_loss_vertex(t_vp, t_vt, t_vw)
     loss_pose = POSE_W * average_distance_loss_func(t_pp, t_pt, t_pw, t_pts, t_sym, num_classes, margin=0.01)
 
+    loss_pose.backward()
+    g_tvp = t_vp.grad.cpu().numpy()
+    assert np.sum(g_tvp) == 0
