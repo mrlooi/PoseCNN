4b9275f4a09c110cddef4d2a87dfde5be77a74b4
vincentlv <looicj@msn.com>
2018-11-20 09:05:14 +0800

    refactored posecnn into submodules per different head

--- my_tools/convert_to_pth.py
+++ my_tools/convert_to_pth.py
@@ -15,6 +15,209 @@ sys.path.append("lib/model")
 from roi_pooling.modules.roi_pool import _RoIPooling
 from hough_voting.modules.hough_voting import HoughVoting
 
+
+def _get_conv_transpose2d(in_cn, out_cn, kernel_size, stride=(1,1), padding=0, trainable=False):
+    x = nn.ConvTranspose2d(in_cn, out_cn, kernel_size=kernel_size, stride=stride, padding=padding)
+    x.bias.data.zero_()
+    # if not trainable:
+    #     for p in x.parameters():
+    #         p.requires_grad = False
+    return x
+
+def _get_fc_layer(in_cn, out_cn):
+    x = nn.Linear(in_cn, out_cn)
+    x.bias.data.zero_()
+    nn.init.normal_(x.weight, 0.0, 0.001)
+    return x
+
+class VGG16(nn.Module):
+    def __init__(self):
+        super(VGG16, self).__init__()
+
+        self.max_pool2d = nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+
+        self.conv_dim_out = [64,128,256,512,512]
+        c_dims = self.conv_dim_out
+
+        inplace = True
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace),
+            nn.Conv2d(64, c_dims[0], kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(c_dims[0], 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace),
+            nn.Conv2d(128, c_dims[1], kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace),
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(c_dims[1], 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace),
+            nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace),
+            nn.Conv2d(256, c_dims[2], kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace),
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(c_dims[2], 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace),
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace),
+            nn.Conv2d(512, c_dims[3], kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace),
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(c_dims[3], 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace),
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace),
+            nn.Conv2d(512, c_dims[4], kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace),
+        )
+
+    def forward(self, x):
+        conv1 = self.conv1(x)
+        conv2 = self.conv2(self.max_pool2d(conv1))
+        conv3 = self.conv3(self.max_pool2d(conv2))
+        conv4 = self.conv4(self.max_pool2d(conv3))
+        conv5 = self.conv5(self.max_pool2d(conv4))
+        return conv4, conv5
+
+    def load_pretrained(self, model_file):
+        m = torch.load(model_file)
+        mk = m.keys()[:26]
+        sd = self.state_dict()
+        sdk = sd.keys()
+
+        print("Loading pretrained model %s..."%(model_file))
+        for ix,k in enumerate(mk):
+            md = m[k]
+            sk = sdk[ix]
+            d = sd[sk]
+            assert d.shape == md.shape
+            print("%s -> %s [%s]"%(k, sk, str(d.shape)))
+            sd[sk] = md
+        self.load_state_dict(sd)
+        print("Loaded pretrained model %s"%(model_file))
+
+class SemanticMaskHead(nn.Module):
+    def __init__(self, conv0_dim_in, conv1_dim_in, dim_out, num_units=64):
+        super(SemanticMaskHead, self).__init__()
+
+        inplace = True
+
+        self.score_conv4 = nn.Sequential(
+            nn.Conv2d(conv0_dim_in, num_units, kernel_size=(1, 1), stride=(1, 1)),
+            nn.ReLU(inplace)
+        )   
+        self.score_conv5 = nn.Sequential(
+            nn.Conv2d(conv1_dim_in, num_units, kernel_size=(1, 1), stride=(1, 1)),
+            nn.ReLU(inplace)
+        )
+        self.upscore_conv5 = _get_conv_transpose2d(num_units, num_units, kernel_size=(4,4), stride=(2,2), padding=(1,1), trainable=False)
+        self.upscore = _get_conv_transpose2d(num_units, num_units, kernel_size=(16,16), stride=(8,8), padding=(4,4), trainable=False)
+        self.score = nn.Sequential(
+            nn.Conv2d(num_units, dim_out, kernel_size=(1, 1), stride=(1, 1)),
+            nn.ReLU(inplace)
+        )
+
+        self.dim_out = dim_out
+
+    def forward(self, conv0, conv1):
+        sc_conv4 = self.score_conv4(conv0)
+        sc_conv5 = self.score_conv5(conv1)
+        upsc_conv5 = self.upscore_conv5(sc_conv5)
+        add_score = torch.add(sc_conv4, upsc_conv5)
+        upscore = self.upscore(add_score)
+        score = self.score(upscore)
+        softmax = F.softmax(score, 1)
+        label2d = torch.argmax(softmax, dim=1)
+        return score, label2d
+
+class VertexRegHead(nn.Module):
+    def __init__(self, conv0_dim_in, conv1_dim_in, dim_out, num_units=128):
+        super(VertexRegHead, self).__init__()
+
+        inplace = True
+
+        self.dim_out = dim_out * 3  # xyz reg
+
+        self.score_conv4_vertex = nn.Conv2d(conv0_dim_in, num_units, kernel_size=(1, 1), stride=(1, 1))
+        self.score_conv5_vertex = nn.Conv2d(conv1_dim_in, num_units, kernel_size=(1, 1), stride=(1, 1))
+        self.upscore_conv5_vertex = _get_conv_transpose2d(num_units, num_units, kernel_size=(4,4), stride=(2,2), padding=(1,1), trainable=False)
+        self.upscore_vertex = _get_conv_transpose2d(num_units, num_units, kernel_size=(16,16), stride=(8,8), padding=(4,4), trainable=False)
+        self.vertex_pred = nn.Conv2d(num_units, self.dim_out, kernel_size=(1, 1), stride=(1, 1))
+
+    def forward(self, conv0, conv1):
+        sc_conv4 = self.score_conv4_vertex(conv0)
+        sc_conv5 = self.score_conv5_vertex(conv1)
+        upsc_conv5 = self.upscore_conv5_vertex(sc_conv5)
+        add_score = torch.add(sc_conv4, upsc_conv5)
+        upscore = self.upscore_vertex(add_score)
+        vertex_pred = self.vertex_pred(upscore)
+        return vertex_pred
+
+class RoiPoolLayer(nn.Module):
+    def __init__(self, roi_pool_dims=(7,7)):
+        super(RoiPoolLayer, self).__init__()    
+        if isinstance(roi_pool_dims, int):
+            roi_pool_dims = (roi_pool_dims, roi_pool_dims)
+        self.roi_pool_dims = roi_pool_dims
+        
+
+class PoseRegHead(nn.Module):
+    def __init__(self, dim_in, dim_out, num_units=4096):
+        super(PoseRegHead, self).__init__()
+
+        inplace = True
+
+        self.dim_in = dim_in
+        self.dim_out = dim_out * 4
+        self.num_units = num_units
+
+        self.poses_fc1 = _get_fc_layer(self.dim_in, num_units)
+        self.poses_fc2 = _get_fc_layer(num_units, num_units)
+        self.poses_fc3 = _get_fc_layer(num_units, self.dim_out)
+
+    def forward(self, x, drop_prob=0.0, is_train=False):
+        x_flat = x.view(-1, self.dim_in)
+
+        fc1 = self.poses_fc1(x_flat)
+        fc1 = F.normalize(fc1, p=2, dim=1)
+        fc1 = F.dropout(F.relu(fc1), drop_prob, training=is_train)
+        fc2 = self.poses_fc2(fc1)
+        fc2 = F.normalize(fc2, p=2, dim=1)
+        fc2 = F.dropout(F.relu(fc2), drop_prob, training=is_train)
+        fc3 = self.poses_fc3(fc2)
+        fc3 = F.normalize(fc3, p=2, dim=1)
+
+        return F.tanh(fc3)
+
+class HoughVoteLayer(nn.Module):
+    def __init__(self, dim_in, threshold_vote, threshold_percentage, label_threshold=500, inlier_threshold=0.9, skip_pixels=1, is_train=False):
+        super(HoughVoteLayer, self).__init__()
+
+        self.dim_in = dim_in
+        self.label_threshold = int(label_threshold)
+        self.inlier_threshold = float(inlier_threshold)
+        self.threshold_vote = float(threshold_vote)
+        self.threshold_percentage = float(threshold_percentage)
+        self.skip_pixels = int(skip_pixels)
+
+        self.is_train = is_train
+
+        self.hough_voting = HoughVoting(self.dim_in, self.threshold_vote, self.threshold_percentage, label_threshold=self.label_threshold, 
+            inlier_threshold=self.inlier_threshold, skip_pixels=self.skip_pixels, is_train=self.is_train)
+
+    def forward(self, label_2d, vertex_pred, extents, poses, mdata):
+        outputs = self.hough_voting(label_2d, vertex_pred.permute((0,2,3,1)).clone(), extents, poses, mdata)
+        # rois_raw = outputs[0]
+
+        return outputs
+
 class PoseCNN(nn.Module):
     def __init__(self, num_units, num_classes, label_threshold=500, vote_threshold=-1.0, is_train=False, keep_prob=1.0):
         super(PoseCNN, self).__init__()
@@ -34,93 +237,39 @@ class PoseCNN(nn.Module):
         drop_prob = 1.0 - keep_prob if is_train else 0.0
         self.drop_prob = drop_prob
 
-        self.max_pool2d = nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-            nn.ReLU(inplace),
-            nn.Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-            nn.ReLU(inplace),
-        )
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-            nn.ReLU(inplace),
-            nn.Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-            nn.ReLU(inplace),
-        )
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(128, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-            nn.ReLU(inplace),
-            nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-            nn.ReLU(inplace),
-            nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-            nn.ReLU(inplace),
-        )
-        self.conv4 = nn.Sequential(
-            nn.Conv2d(256, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-            nn.ReLU(inplace),
-            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-            nn.ReLU(inplace),
-            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-            nn.ReLU(inplace),
-        )
-        self.conv5 = nn.Sequential(
-            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-            nn.ReLU(inplace),
-            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-            nn.ReLU(inplace),
-            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-            nn.ReLU(inplace),
-        )
+        self.features = VGG16()
+        dim_outs = self.features.conv_dim_out
+        dim_out_0, dim_out_1 = dim_outs[-2:]
 
-        # self.features = nn.Sequential(
-        #     self.conv1, self.max_pool2d, 
-        #     self.conv2, self.max_pool2d, 
-        #     self.conv3, self.max_pool2d,
-        #     self.conv4, self.max_pool2d,
-        #     self.conv5
-        # )
-        
         """
         SEMANTIC MASK LAYER
         """
-        self.score_conv4 = nn.Sequential(
-            nn.Conv2d(512, num_units, kernel_size=(1, 1), stride=(1, 1)),
-            nn.ReLU(inplace)
-        )   
-        self.score_conv5 = nn.Sequential(
-            nn.Conv2d(512, num_units, kernel_size=(1, 1), stride=(1, 1)),
-            nn.ReLU(inplace)
-        )
-        self.upscore_conv5 = self._get_conv_transpose2d(num_units, num_units, kernel_size=(4,4), stride=(2,2), padding=(1,1), trainable=False)
-        self.upscore = self._get_conv_transpose2d(self.num_units, self.num_units, kernel_size=(16,16), stride=(8,8), padding=(4,4), trainable=False)
-        self.score = nn.Sequential(
-            nn.Conv2d(self.num_units, self.num_classes, kernel_size=(1, 1), stride=(1, 1)),
-            nn.ReLU(inplace)
-        )
+        self.mask_head = SemanticMaskHead(dim_out_0, dim_out_1, num_classes, num_units)
 
         """
         VERTEX REG LAYER
         """
-        self.score_conv4_vertex = nn.Conv2d(512, 128, kernel_size=(1, 1), stride=(1, 1))
-        self.score_conv5_vertex = nn.Conv2d(512, 128, kernel_size=(1, 1), stride=(1, 1))
-        self.upscore_conv5_vertex = self._get_conv_transpose2d(128, 128, kernel_size=(4,4), stride=(2,2), padding=(1,1), trainable=False)
-        self.upscore_vertex = self._get_conv_transpose2d(128, 128, kernel_size=(16,16), stride=(8,8), padding=(4,4), trainable=False)
-        self.vertex_pred = nn.Conv2d(128, 3 * self.num_classes, kernel_size=(1, 1), stride=(1, 1))
+        self.vertex_head = VertexRegHead(dim_out_0, dim_out_1, num_classes, num_units*2)
 
-        self.hough_voting = HoughVoting(self.num_classes, self.vote_threshold, self.vote_percentage, label_threshold=self.label_threshold, 
+        """
+        HOUGH VOTE LAYER
+        """
+        self.hough_voting = HoughVoteLayer(self.num_classes, self.vote_threshold, self.vote_percentage, label_threshold=self.label_threshold, 
             inlier_threshold=self.inlier_threshold, skip_pixels=self.skip_pixels, is_train=self.is_train)
 
         """
-        POSE REG LAYER
+        ROI POOL LAYER
         """
         self.roi_pool_dims = (7,7)
         h, w = self.roi_pool_dims
         self.roi_pool1 = _RoIPooling(h,w,1.0/16)  # from conv5_3
         self.roi_pool2 = _RoIPooling(h,w,1.0/8)  # from conv4_3
 
-        self.poses_fc1 = self._get_fc_layer(h*w*512,4096)
-        self.poses_fc2 = self._get_fc_layer(4096,4096)
-        self.poses_fc3 = self._get_fc_layer(4096, 4*self.num_classes)
+        """
+        POSE REG LAYER
+        """
+        assert dim_out_0 == dim_out_1
+        self.pose_head = PoseRegHead(h*w*dim_out_0, num_classes, 4096)
 
         # self.poses_fc = nn.Sequential(
         #     # nn.Linear(h*w*512,4096),
@@ -135,52 +284,6 @@ class PoseCNN(nn.Module):
         #     nn.Linear(4096,4 * self.num_classes)
         # )
 
-
-    def features(self, x):
-        conv1 = self.conv1(x)
-        conv2 = self.conv2(self.max_pool2d(conv1))
-        conv3 = self.conv3(self.max_pool2d(conv2))
-        conv4 = self.conv4(self.max_pool2d(conv3))
-        conv5 = self.conv5(self.max_pool2d(conv4))
-        return conv4, conv5
-
-    def _get_conv_transpose2d(self, in_cn, out_cn, kernel_size, stride=(1,1), padding=0, trainable=False):
-        x = nn.ConvTranspose2d(in_cn, out_cn, kernel_size=kernel_size, stride=stride, padding=padding)
-        x.bias.data.zero_()
-        # if not trainable:
-        #     for p in x.parameters():
-        #         p.requires_grad = False
-        return x
-
-    def _get_fc_layer(self, in_cn, out_cn):
-        x = nn.Linear(in_cn, out_cn)
-        x.bias.data.zero_()
-        nn.init.normal_(x.weight, 0.0, 0.001)
-        return x
-
-    def semantic_mask_layer(self, conv4, conv5):
-        sc_conv4 = self.score_conv4(conv4)
-        sc_conv5 = self.score_conv5(conv5)
-        upsc_conv5 = self.upscore_conv5(sc_conv5)
-        add_score = torch.add(sc_conv4, upsc_conv5)
-        upscore = self.upscore(add_score)
-        score = self.score(upscore)
-        softmax = F.softmax(score, 1)
-        label2d = torch.argmax(softmax, dim=1)
-        return score, label2d
-
-    def vertex_reg_layer(self, conv4, conv5):
-        sc_conv4 = self.score_conv4_vertex(conv4)
-        sc_conv5 = self.score_conv5_vertex(conv5)
-        upsc_conv5 = self.upscore_conv5_vertex(sc_conv5)
-        add_score = torch.add(sc_conv4, upsc_conv5)
-        upscore = self.upscore_vertex(add_score)
-        vertex_pred = self.vertex_pred(upscore)
-        return vertex_pred
-
-    def run_hough_voting(self, label_2d, vertex_pred, extents, poses, mdata):
-        # need to permute vertex_pred from (B,C,H,W) to (B,H,W,C)
-        return self.hough_voting(label_2d, vertex_pred.permute((0,2,3,1)).clone(), extents, poses, mdata)
 
     def pose_reg_layer(self, conv4, conv5, rois_raw):
         # rois_raw from hough_voting: batch_index, cls, x1, y1, x2, y2, max_hough_idx
@@ -198,31 +301,22 @@ class PoseCNN(nn.Module):
         rp2 = self.roi_pool2(conv4, rois)  # B,C,H,W  (B,512,7,7)
         rp = torch.add(rp1, rp2)
         # print(rp.shape)
-        rp_flat = rp.view(-1, h*w*512)
-        # fc = self.poses_fc(rp_flat)
-        fc1 = self.poses_fc1(rp_flat)
-        fc1 = F.normalize(fc1, p=2, dim=1)
-        fc1 = F.dropout(F.relu(fc1), self.drop_prob, training=self.is_train)
-        fc2 = self.poses_fc2(fc1)
-        fc2 = F.normalize(fc2, p=2, dim=1)
-        fc2 = F.dropout(F.relu(fc2), self.drop_prob, training=self.is_train)
-        fc3 = self.poses_fc3(fc2)
-        fc3 = F.normalize(fc3, p=2, dim=1)
+        out = self.pose_head(rp, self.drop_prob, self.is_train)
 
-        return F.tanh(fc3)
+        return out
 
     def forward_image(self, x):
         c4, c5 = self.features(x)
-        _, label_2d = self.semantic_mask_layer(c4, c5)
-        vertex_pred = self.vertex_reg_layer(c4, c5)
+        _, label_2d = self.mask_head(c4, c5)
+        vertex_pred = self.vertex_head(c4, c5)
 
         return label_2d, vertex_pred
 
     def forward(self, x, extents, poses, mdata):
         c4, c5 = self.features(x)
-        score, label_2d = self.semantic_mask_layer(c4, c5)
-        vertex_pred = self.vertex_reg_layer(c4, c5)
-        hough_outputs = self.run_hough_voting(label_2d, vertex_pred, extents, poses, mdata)
+        score, label_2d = self.mask_head(c4, c5)
+        vertex_pred = self.vertex_head(c4, c5)
+        hough_outputs = self.hough_voting(label_2d, vertex_pred, extents, poses, mdata)
         rois = hough_outputs[0]
         pose_reg = self.pose_reg_layer(c4, c5, rois)
         # out = out.view(out.size(0), -1)
@@ -230,21 +324,7 @@ class PoseCNN(nn.Module):
         return score, label_2d, vertex_pred, hough_outputs, pose_reg
 
     def load_pretrained(self, model_file):
-        m = torch.load(model_file)
-        mk = m.keys()[:26]
-        sd = self.state_dict()
-        sdk = sd.keys()
-
-        print("Loading pretrained model %s..."%(model_file))
-        for ix,k in enumerate(mk):
-            md = m[k]
-            sk = sdk[ix]
-            d = sd[sk]
-            assert d.shape == md.shape
-            print("%s -> %s [%s]"%(k, sk, str(d.shape)))
-            sd[sk] = md
-        self.load_state_dict(sd)
-        print("Loaded pretrained model %s"%(model_file))
+        self.features.load_pretrained(model_file)
 
 def posecnn_pth_to_tf_mapping():
     return {
